a116701b75f4b22676ba2ffc43cab2755e4e729c
THTBSE <fuchilin90@gmail.com>
2015-09-05 22:37:49 +0800

    增加信息增益计算

--- tools/feature_selection.py
+++ tools/feature_selection.py
@@ -2,9 +2,11 @@ import math
 
 class WordStatis():
 	def __init__(self):
-		self.tf_idf = {}
+		self.tf_idf = {} #using for set weight for feature words 
 		self.class_word_df = {} #document frequency of word in every class
 		self.chi_square = {}
+		self.information_gain = {}
+		self._all_words = set()
 
 	def tf(self, docs):
 		"""term frequency of words"""
@@ -44,7 +46,7 @@ class WordStatis():
 			self.tf_idf[word]['tf_idf'] = self.tf_idf[word]['tf'] * self.tf_idf[word]['idf']
 
 	def calc_class_word_df(self, docs_with_label):
-		"""chi-square of words"""
+		"""calc every word's document frequency in every class"""
 		for doc,label in docs_with_label:
 			#chi-square test doesn't consider term frequency
 			unique_words = set(doc)
@@ -56,15 +58,17 @@ class WordStatis():
 				self.class_word_df[label].setdefault(word, 0.0)
 				self.class_word_df[label][word] += 1.0
 
-				self.chi_square.setdefault(word, {})
-				self.chi_square[word].setdefault(label, 0.0)
+				self._all_words.add(word)
 
 	def calc_x2_of_class(self, class_label):
 		"""calc chi-square of words"""
 		if class_label not in self.class_word_df:
 			return None
 
-		other_class = filter(lambda y:y != class_label, self.class_word_df)
+		all_classes = self.class_word_df.keys()
+		self.chi_square = {w:{c:0.0 for c in all_classes} for w in self._all_words}
+		other_class = filter(lambda y:y != class_label, all_classes)
+
 		for word in self.chi_square:
 			A = B = C = D = 0.0
 			if word in self.class_word_df[class_label]:
@@ -81,8 +85,54 @@ class WordStatis():
 			N = A + B + C + D
 			self.chi_square[word][class_label] = (N*((A*D - B*C)**2)) / ((A+C)*(A+B)*(B+D)*(C+D))
 
+	def calc_information_gain(self):
+		"""calc IG for features, the class_word_df should be calc first"""
+		if not self.class_word_df:
+			return None
+
+		all_classes = self.class_word_df.keys()
+		prob_class = {k:0.0 for k in all_classes}
+		all_docs = 0
+
+		#estimate probability of each class
+		for cls in all_classes:
+			clsCount = self.class_word_df[cls]['_class_count_']
+			prob_class[cls] += clsCount
+			all_docs += clsCount
+
+		all_docs = float(all_docs)
+		for cls in all_classes:
+			prob_class[cls] /= all_docs
+
+		#entropy of original dataset
+		entropy = 0.0
+		for cls in all_classes:
+			entropy -= (prob_class[cls] * math.log(prob_class[cls])) 
+
+		for word in self._all_words:
+			#p(w) and p(_w)
+			prob_word = 0.0
+			num_word = 0.0
+			for cls in all_classes:
+				num_word += self.class_word_df[cls][word]
+			prob_word = num_word / all_docs
+			num_non_word = all_docs - num_word
+			prob_non_word = 1 - prob_word
+
+			#p(c|w) and p(c|_w)
+			prob_word_class = {k:0.0 for k in all_classes}
+			prob_non_word_class = {k:0.0 for k in all_classes}
+
+			for cls in all_classes:
+				prob_word_class[cls] = self.class_word_df[cls][word] / num_word
+				prob_non_word_class[cls] = (self.class_word_df[cls]['_class_count_'] - self.class_word_df[cls][word]) / num_non_word
 
 
+			entropy_word = entropy_non_word = 0.0
+			for cls in all_classes:
+				entropy_word -= (prob_word_class[cls] * math.log(prob_word_class[cls]))
+				entropy_non_word -= (prob_non_word_class[cls] * math.log(prob_non_word_class))
 
-
+			#IG = H(X) - H(C|X)
+			self.information_gain[word] = entropy - (entropy_word + entropy_non_word)
 						
